0cdafa08b298fde3b786c37653bf8b3131a35e2d
Sawelch2027 <Sawelch@email.sc.edu>
2025-02-26 21:16:56 -0500

    Music Progression done

--- easymusicj/src/main/java/module-info.java
+++ easymusicj/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.easymusic {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires jfugue;
 
     opens com.easymusic to javafx.fxml;
     exports com.easymusic;
