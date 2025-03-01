fdde466fe93208966ba503d8b172a1fa629986ba
BrysonS1014 <bryson.i.sinclair@gmail.com>
2025-02-28 19:20:27 -0500

    Created Lesson stub #33

---
+++ easymusicj/src/main/java/com/model/Lesson.java
@@ -0,0 +1,25 @@
+package com.model;
+
+public class Lesson {
+
+    private String title;
+    private boolean completed = false;
+
+    public Lesson(String title) {
+        this.title = title;
+    }
+
+    public void startLesson() {
+        System.out.println("Lesson started: " + title);
+    }
+
+    public void completeLesson() {
+        completed = true;
+        System.out.println("Lesson completed!");
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+    
+}
