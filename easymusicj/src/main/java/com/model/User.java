7bcf51ae03f401fbcc63e4ef123fbedbe527159f
BrysonS1014 <bryson.i.sinclair@gmail.com>
2025-03-18 12:05:50 -0400

    Added Getters and Setters, Added more parameters in the User Constructor

--- easymusicj/src/main/java/com/model/User.java
+++ easymusicj/src/main/java/com/model/User.java
@@ -12,12 +12,15 @@ public class User {
     private Instrument selectedInstrument;
     private List<Song> composedSongs;
 
-    public User(String username, String password, String email, String firstName, String lastName) {
+    public User(String username, String password, String email, String firstName, String lastName, int practiceStreak, Instrument selectedInstrument, List<Song> composedSongs) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.practiceStreak = 0;
+        this.selectedInstrument = selectedInstrument;
+        this.composedSongs = composedSongs;
     }
 
     public String getUsername() {
@@ -38,4 +41,66 @@ public class User {
         practiceStreak++;
         System.out.println(username + " practiced! Current streak: " + practiceStreak);
     }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPracticeStreak() {
+        return practiceStreak;
+    }
+
+    public void setPracticeStreak(int practiceStreak) {
+        this.practiceStreak = practiceStreak;
+    }
+
+    public Instrument getSelectedInstrument() {
+        return selectedInstrument;
+    }
+
+    public void setSelectedInstrument(Instrument selectedInstrument) {
+        this.selectedInstrument = selectedInstrument;
+    }
+
+    public List<Song> getComposedSongs() {
+        return composedSongs;
+    }
+
+    public void setComposedSongs(List<Song> composedSongs) {
+        this.composedSongs = composedSongs;
+    }
+
+    
 }
