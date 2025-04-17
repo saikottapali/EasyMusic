9d695cdd209358245a0705c46696ddf064ed71b1
Sawelch2027 <Sawelch@email.sc.edu>
2025-04-17 11:45:37 -0400

    playsong is implemented working on making it show up

--- src/main/java/com/easymusicj/DashboardController.java
+++ src/main/java/com/easymusicj/DashboardController.java
@@ -12,6 +12,9 @@ public class DashboardController {
     @FXML
     private Button btn_logOut;
 
+    @FXML
+    private Button btn_playSong;
+
 
     @FXML
     private void handleLogOut() throws IOException{
@@ -19,5 +22,12 @@ public class DashboardController {
         Stage stage = (Stage) btn_logOut.getScene().getWindow();
         App.setRoot("login");
 
+    }
+
+    @FXML
+    private void playSong() throws IOException {
+        // Add logic to play a song
+        Stage stage = (Stage) btn_playSong.getScene().getWindow();
+        App.setRoot("playsong");
     }
 }
